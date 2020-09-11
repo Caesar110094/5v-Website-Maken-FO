4c1c349df0c29d0cdc7b4dc8099613fbc1666e41
youri wessels <110158@st-maartenscollege.nl>
2020-09-11 10:06:50 +0200

    iets vergeten

--- mvc-template-master/SchoolTemplate/Models/PersonModel.cs
+++ mvc-template-master/SchoolTemplate/Models/PersonModel.cs
@@ -3,13 +3,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SchoolTemplate.Models
 {
-  public class PersonModel
+    public class PersonModel
     {
         public string Voornaam { get; set; }
         public string Achternaam { get; set; }
         public DateTime Geboortedatum { get; set; }
-        [Required(ErrorMessage ="E-mail is verplicht")]
-        [EmailAddress(ErrorMessage ="Geen bestaand E-mailadres")]
+        [Required(ErrorMessage = "E-mail is verplicht")]
+        [EmailAddress(ErrorMessage = "Geen bestaand E-mailadres")]
         public string Email { get; set; }
         public string Telefoonnummer { get; set; }
     }
+}
