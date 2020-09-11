b001d5e4966cf1af28b22787f6614c240672a1c5
Caesar Coolen <110094@st-maartenscollege.nl>
2020-09-11 14:56:19 +0200

    Div class for form

--- mvc-template-master/SchoolTemplate/Models/PersonModel.cs
+++ mvc-template-master/SchoolTemplate/Models/PersonModel.cs
@@ -4,12 +4,13 @@ using System.ComponentModel.DataAnnotations;
 namespace SchoolTemplate.Models
 {
   public class PersonModel
-    {
-        public string Voornaam { get; set; }
-        public string Achternaam { get; set; }
-        public DateTime Geboortedatum { get; set; }
-        [Required(ErrorMessage ="E-mail is verplicht")]
-        [EmailAddress(ErrorMessage ="Geen bestaand E-mailadres")]
-        public string Email { get; set; }
-        public string Telefoonnummer { get; set; }
-    }
+  {
+    public string Voornaam { get; set; }
+    public string Achternaam { get; set; }
+    public DateTime Geboortedatum { get; set; }
+    [Required(ErrorMessage = "E-mail is verplicht")]
+    [EmailAddress(ErrorMessage = "Geen bestaand E-mailadres")]
+    public string Email { get; set; }
+    public string Telefoonnummer { get; set; }
+  }
+}
